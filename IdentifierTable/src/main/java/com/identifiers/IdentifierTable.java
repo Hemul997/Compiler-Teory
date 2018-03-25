632ebbce2d50a326949494daeffefbeee965c977
Hemul997 <nikitamoshkin1997@gmail.com>
2018-03-25 13:02:29 +0300

    Добавил таблицу типов (Тип, размер).

--- IdentifierTable/src/main/java/com/identifiers/IdentifierTable.java
+++ IdentifierTable/src/main/java/com/identifiers/IdentifierTable.java
@@ -8,15 +8,33 @@ import java.util.TreeMap;
 
 public class IdentifierTable {
     private Map<String, IdentifierData> identifiers = new TreeMap<>();
+    private Map<String, Integer> data = new TreeMap<>();
 
-    public void putID(String idName, IdentifierData data) throws ExistIdInTableException {
-        if (this.identifiers.containsKey(idName))
-            throw new ExistIdInTableException();
-        else
-            this.identifiers.put(idName, data);
+    public IdentifierTable () {
+        initData();
     }
 
-    public IdentifierData getIDData (String idName) throws NotFoundIdInTableException {
+    private void initData() {
+        data.put(String.valueOf(Long.TYPE), Long.BYTES);
+        data.put(String.valueOf(Integer.TYPE), Integer.BYTES);
+        data.put(String.valueOf(Byte.TYPE), Byte.BYTES);
+        data.put(String.valueOf(Double.TYPE), Double.BYTES);
+        data.put(String.valueOf(Float.TYPE), Float.BYTES);
+        data.put(String.valueOf(Character.TYPE), Character.BYTES);
+        data.put(String.valueOf(Boolean.TYPE), 1);
+    }
+
+    public void put(String idName, String type) throws ExistIdInTableException {
+        if (this.identifiers.containsKey(idName))
+            throw new ExistIdInTableException();
+        else {
+            IdentifierData data = new IdentifierData(type, this.data.get(type));
+            this.identifiers.put(idName, data);
+        }
+
+    }
+
+    public IdentifierData get(String idName) throws NotFoundIdInTableException {
         if (!this.identifiers.containsKey(idName))
             throw new NotFoundIdInTableException();
         else
